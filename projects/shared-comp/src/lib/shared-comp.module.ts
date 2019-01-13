248e4030d81d74b90a68f7b6e1fa3476c229b518
thomascsd <thomascsd808@gmail.com>
2019-01-13 23:28:11 +0800

    建立Lib

---
+++ projects/shared-comp/src/lib/shared-comp.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [],
+  exports: [HeaderComponent]
+})
+export class SharedCompModule {}
---
+++ projects/shared-comp/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of shared-comp
+ */
+
+export * from './lib/shared-comp.service';
+export * from './lib/shared-comp.component';
+export * from './lib/shared-comp.module';
